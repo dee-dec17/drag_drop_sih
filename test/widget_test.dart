ad9356e35dd3b2c578412035e77136646c2b18c9
dee-dec17 <deekshitha.kolusu@gmail.com>
2020-08-04 09:50:48 +0530

    minor changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:drag_drop_game/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(DragDrop());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
